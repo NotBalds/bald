076528022769e5215e6dbfc7251b375f3e13a10d
ProggerX <x@proggers.ru>
2024-01-10 19:07:11 +0300

    Added project open

--- src/project.cpp
+++ src/project.cpp
@@ -4,23 +4,39 @@
 
 using namespace std;
 
+
+
 class ProjectParser {
 private:
+	string _projects_path;
 	void new_project(argh::parser parser) {
 		string name = parser[3];
 
 		cout << "Executing mkdir...\n";
-		system(("mkdir -p " + name + "/src/").c_str());
+		system(("mkdir -p " + _projects_path + name + "/src/").c_str());
 		cout << "Changing dir...\n";
-		chdir(name.c_str());
+		chdir((_projects_path + name).c_str());
 		cout << "Creating code and baldfile\n";
 		system("touch src/main.cpp baldfile");
-		cout << "Done! Use 'cd " + name + "' to go to project directory\n";
+		cout << "Done! Use 'bald project open " + name + "' or 'cd " + _projects_path + name + "' to open project\n";
+	}
+	void open_project(argh::parser parser) {
+		string name = parser[3];
+		
+		chdir((_projects_path + name).c_str());
+		system("$SHELL");
 	}
 public:
+	ProjectParser() {
+		_projects_path = getenv("BALD");
+		system(("mkdir -p " + _projects_path).c_str());
+	}
 	void parse(argh::parser parser) { 
 		if (parser[2] == "new") {
 			new_project(parser);
+		}
+		else if (parser[2] == "open") {
+			open_project(parser);
 		}
 	}
 };
