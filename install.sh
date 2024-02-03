cad8c32b998a25a7568f564604ee681066e816f2
TBWanderer <x@no-bot.pw>
2024-02-03 18:49:41 +0300

    Added strange beauty

--- install.sh
+++ install.sh
@@ -12,30 +12,30 @@ echo "|_______| \\__/|_| \\_\\ \\____/|_|"
 sleep 1
 
 echo ""
-echo "Welcome to the club, dude -_-"
+echo "[?] Welcome to the club, dude -_-"
 sleep 2
-echo "Bald is c++ project building system." 
+echo "[?] Bald is c++ project building system." 
 sleep 3
-echo "It uses TOML"
+echo "[?] It uses TOML"
 sleep 1
-echo "Let me install it for you"
+echo "[?] Let me install it for you"
 sleep 3
-echo "First, let's check dependencies"
+echo "[.] First, let's check dependencies"
 (g++ --version > /dev/null &&
 make --version > /dev/null &&
 git --version > /dev/null) &&
 (
 sleep 3
-echo "You have dependencies"
+echo "[!] You have dependencies"
 sleep 2
-echo "Then, let's clone the repo"
+echo "[.] Then, let's clone the repo"
 sleep 3
 git clone https://github.com/NotBalds/bald
 sleep 2
-echo "cd..."
+echo "[.] cd..."
 cd bald
 sleep 2
-echo "And run make"
+echo "[.] And run make"
 make
 sleep 3
 echo ""
@@ -44,5 +44,5 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo "Installation finished, read https://github.com/NotBalds/bald/blob/README.md for usage!")
+echo "[!] Installation finished, read https://github.com/NotBalds/bald/blob/README.md for usage!")
 sleep 5
