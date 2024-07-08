d8d32cc019b1a411bda7d184428e7ea0c4c8cb33
ProggerX <x@proggers.ru>
2024-07-08 10:50:33 +0300

    Added systems

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 	};
 	outputs = { nixpkgs, ... }:
-	let systems = [ "x86_64-linux" "aarch64-linux" ];
+	let systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 	in {
 		defaultPackage = builtins.listToAttrs (builtins.map (system: { name = system; value =
 		let pkgs = import nixpkgs { inherit system; };
